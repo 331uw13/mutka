49eb31b42bff7224a5da95ee95c5040b2d8e57ce
331uw13 <eeiuwie@gmail.com>
2025-11-30 21:45:34 +0200

    Messed up gitignore

---
+++ examples/server/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+
+FIL_C=$HOME/Github/fil-c-0.675
+OPENSSL=../../external/openssl
+
+CC=$FIL_C/build/bin/clang
+
+
+SRC_FILES=$(find ./src -type f -name *.c)
+
+
+
+$CC -Wall -Wextra $SRC_FILES -I$OPENSSL\
+    -L../../libmutka -lmutka -o server.example
+
+
